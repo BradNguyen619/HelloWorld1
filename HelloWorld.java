8d6a1d722b1a6519c103e56ca9df5c38ddbc69e2
Brad Nguyen <brad.nguyen@mrjaffesclass.com>
2023-09-06 11:19:32 -0700

    DataTypes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World!") ;
+        int i;
+        double f = 2.5;
+        boolean flag = true;
     }
 }
